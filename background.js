37c78d1e59ad4bb319bb5d6e65238217e594a92a
Mwiku <mwizak@gmail.com>
2024-06-11 07:54:40 +0200

    fix: update popup, fix bug where tab details r undefined

--- background.js
+++ background.js
@@ -1,11 +1,29 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (tab.url.startsWith("http://l:") && changeInfo.status === "complete") {
-        const port = tab.url.slice(9);
-        redirectUrl(port);
-    } else if (tab.title.startsWith("l:") && changeInfo.status === "complete") {
-        // This one specifically covers for port 80
-        const port = tab.title.slice(2, 5);
-        redirectUrl(port);
+chrome.tabs.onUpdated.addListener((changeInfo, tab) => {
+    console.log(`tab info: ${JSON.stringify(tab)}`);
+    try {
+        // if (
+        //     changeInfo.status === "complete" &&
+        //     (tab.url == undefined || tab.title == undefined)
+        // ) {
+        //     console.log(` changeinfo : ${JSON.stringify(tab)}`);
+        //     throw new Error("tab url or tab title is undefined");
+        // }
+        if (
+            changeInfo.status === "complete" &&
+            tab.url.startsWith("http://l:")
+        ) {
+            const port = tab.url.slice(9);
+            redirectUrl(port);
+        } else if (
+            changeInfo.status === "complete" &&
+            tab.title.startsWith("l:")
+        ) {
+            // This one specifically covers for port 80
+            const port = tab.title.slice(2, 5);
+            redirectUrl(port);
+        }
+    } catch (error) {
+        console.log(`Undefined tab url or title ${error}`);
     }
 });
 
